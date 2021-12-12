ee881d5a1638b4694b92c121258a41440b16184e
SergeyDorokhov <dorokhov-sergey@yandex.ru>
2021-12-12 21:16:08 +0300

    1.6. Взаимодействие объектов [#166087]

---
+++ src/main/java/ru/job4j/oop/fairytale/Ball.java
@@ -0,0 +1,12 @@
+package ru.job4j.oop.fairytale;
+
+public class Ball {
+    public void tryRun(boolean condition) {
+        if (condition) {
+            System.out.println("Сьели колобка");
+        } else {
+            System.out.println("Убежал колобок");
+        }
+    }
+
+}
---
+++ src/main/java/ru/job4j/oop/fairytale/Wolf.java
@@ -0,0 +1,7 @@
+package ru.job4j.oop.fairytale;
+
+public class Wolf {
+    public void tryEat(Ball ball) {
+        ball.tryRun(false);
+    }
+}
