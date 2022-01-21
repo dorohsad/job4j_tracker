1bea550c3b0e592bed83afec7d273a13114d2293
SergeyDorokhov <dorohsad@mail.ru>
2022-01-21 14:24:27 +0300

    fix_2. Комбинированный компаратор [#10036 #253705]

--- src/test/java/ru/job4j/collection/JobSortTest.java
+++ src/test/java/ru/job4j/collection/JobSortTest.java
@@ -8,13 +8,12 @@ import java.util.List;
 import static org.junit.Assert.assertArrayEquals;
 
 public class JobSortTest {
-    Job aa2 = new Job("AA", 2);
-    Job ba3 = new Job("BA", 3);
-    Job ca1 = new Job("CA", 1);
-    Job aa1 = new Job("AA", 1);
 
     @Test
     public void whenSortAscByName() {
+        Job aa2 = new Job("AA", 2);
+        Job ba3 = new Job("BA", 3);
+        Job ca1 = new Job("CA", 1);
         List<Job> list = Arrays.asList(ca1, aa2, ba3);
         list.sort(new JobAscByName());
         List<Job> expected = Arrays.asList(aa2, ba3, ca1);
@@ -23,6 +22,9 @@ public class JobSortTest {
 
     @Test
     public void whenSortDescByName() {
+        Job aa2 = new Job("AA", 2);
+        Job ba3 = new Job("BA", 3);
+        Job ca1 = new Job("CA", 1);
         List<Job> list = Arrays.asList(ca1, aa2, ba3);
         list.sort(new JobDescByName());
         List<Job> expected = Arrays.asList(ca1, ba3, aa2);
@@ -31,6 +33,9 @@ public class JobSortTest {
 
     @Test
     public void whenSortAscByPriority() {
+        Job aa2 = new Job("AA", 2);
+        Job ba3 = new Job("BA", 3);
+        Job ca1 = new Job("CA", 1);
         List<Job> list = Arrays.asList(ca1, aa2, ba3);
         list.sort(new JobAscPriority());
         List<Job> expected = Arrays.asList(ca1, aa2, ba3);
@@ -39,6 +44,9 @@ public class JobSortTest {
 
     @Test
     public void whenSortDescByPriority() {
+        Job aa2 = new Job("AA", 2);
+        Job ba3 = new Job("BA", 3);
+        Job ca1 = new Job("CA", 1);
         List<Job> list = Arrays.asList(ca1, aa2, ba3);
         list.sort(new JobDescPriority());
         List<Job> expected = Arrays.asList(ba3, aa2, ca1);
@@ -47,6 +55,10 @@ public class JobSortTest {
 
     @Test
     public void whenSortAscByNameAscByPriority() {
+        Job aa2 = new Job("AA", 2);
+        Job ba3 = new Job("BA", 3);
+        Job ca1 = new Job("CA", 1);
+        Job aa1 = new Job("AA", 1);
         List<Job> list = Arrays.asList(ca1, aa2, aa1, ba3);
         list.sort(new JobAscByName().thenComparing(new JobAscPriority()));
         List<Job> expected = Arrays.asList(aa1, aa2, ba3, ca1);
@@ -55,6 +67,10 @@ public class JobSortTest {
 
     @Test
     public void whenSortAscByNameDescByPriority() {
+        Job aa2 = new Job("AA", 2);
+        Job ba3 = new Job("BA", 3);
+        Job ca1 = new Job("CA", 1);
+        Job aa1 = new Job("AA", 1);
         List<Job> list = Arrays.asList(ca1, aa2, aa1, ba3);
         list.sort(new JobAscByName().thenComparing(new JobDescPriority()));
         List<Job> expected = Arrays.asList(aa2, aa1, ba3, ca1);
@@ -63,6 +79,10 @@ public class JobSortTest {
 
     @Test
     public void whenSortDescByNameAscByPriority() {
+        Job aa2 = new Job("AA", 2);
+        Job ba3 = new Job("BA", 3);
+        Job ca1 = new Job("CA", 1);
+        Job aa1 = new Job("AA", 1);
         List<Job> list = Arrays.asList(ca1, aa2, aa1, ba3);
         list.sort(new JobDescByName().thenComparing(new JobAscPriority()));
         List<Job> expected = Arrays.asList(ca1, ba3, aa1, aa2);
@@ -71,6 +91,10 @@ public class JobSortTest {
 
     @Test
     public void whenSortDescByNameDescByPriority() {
+        Job aa2 = new Job("AA", 2);
+        Job ba3 = new Job("BA", 3);
+        Job ca1 = new Job("CA", 1);
+        Job aa1 = new Job("AA", 1);
         List<Job> list = Arrays.asList(ca1, aa2, aa1, ba3);
         list.sort(new JobDescByName().thenComparing(new JobDescPriority()));
         List<Job> expected = Arrays.asList(ca1, ba3, aa2, aa1);
